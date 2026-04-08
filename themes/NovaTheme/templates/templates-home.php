eedc9c72353948f65a554f0899a39813b9b37e45
Alex <connectionwithme1@gmail.com>
2026-04-08 18:51:18 +0300

    Add color variables and header styles 	- add new SCSS color variables 	- style header menu (layout, links, active state) 	- apply flexbox to body/main/footer for sticky footer 	- update .container padding and centering 	- add templates-home.php for custom home page

---
+++ themes/NovaTheme/templates/templates-home.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+
+ * Template Name: Home Page
+
+ */
+
+get_header(); ?>
+
+<main>
+    <section class="home-hero">
+        <div class="container">
+
+            <?php the_content(); ?>
+
+        </div>
+
+    </section>
+</main>
+
+
+<?php get_footer(); ?>
+
