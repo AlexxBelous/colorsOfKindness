0d61b280112b870e3065b5db6d756f7b3203aba0
Alex <connectionwithme1@gmail.com>
2026-05-10 19:43:36 +0300

    Done: Implement Program Goals and expand UI Elements 	- Added "Program Goals" section with repeater cards and background settings. 	- Enhanced "UI Elements" with conditional logic for Clouds, Buttons, and Brushes. 	- Updated ACF field layouts and wrapper widths for better admin UX. 	- Created program-goals.php and linked it to the Home template. 	- Corrected file modes for ACF JSON configs.

--- themes/NovaTheme/parts/program-goals.php
+++ themes/NovaTheme/parts/program-goals.php
@@ -1,1 +1,11 @@
 <?php
+//$element_type = get_field('ui_elements_element_type');
+//
+//
+//$ui_elements_brush = get_field('ui_elements_brush');
+//dump($ui_elements_brush);
+?>
+
+<section class="programs-goals">
+    
+</section>
--- themes/NovaTheme/templates/templates-home.php
+++ themes/NovaTheme/templates/templates-home.php
@@ -12,6 +12,7 @@ get_header(); ?>
         </div>
     </section>
     <?php get_template_part('parts/home', 'intro'); ?>
+    <?php get_template_part('parts/program', 'goals'); ?>
 </main>
 
 
