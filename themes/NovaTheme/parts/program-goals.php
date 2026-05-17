0a86ba69e6337fe8f99e867ff4463571b11b29d1
Alex <connectionwithme1@gmail.com>
2026-05-17 19:16:42 +0300

    Done: Refactor Program Goals to Mobile First and update brush logic 	- Refactored SCSS to Mobile First and optimized responsive grid. 	- Implemented adaptive <picture> for Desktop/Mobile brush images. 	- Fixed decoration positioning using 'vw' units and centering. 	- Added 'pointer-events: none' to background elements. 	- Split ACF Brush fields and updated SCSS breakpoint examples.

--- themes/NovaTheme/parts/program-goals.php
+++ themes/NovaTheme/parts/program-goals.php
@@ -2,19 +2,26 @@
     <?php
     $ui_elements = get_field('ui_elements');
 
-    if ($ui_elements && $ui_elements['element_type'] == 'Elements of strokes') {
+    if ($ui_elements && $ui_elements['element_type'] === 'Elements of strokes') :
         $brush = $ui_elements['brush'];
-        if (!empty($brush['brush_image'])) {
-            $image_html = wp_get_attachment_image(
-                    $brush['brush_image']['ID'],
-                    'full',
-                    false,
-                    array('class' => 'decoration-img')
-            );
-            echo $image_html;
-        }
-    }
-    ?>
+        $desktop_img = $brush['brush_image_desktop'] ?? null;
+        $mobile_img  = $brush['brush_image_mobile'] ?? null;
+
+        if ($desktop_img) : ?>
+            <picture class="programs-goals__decoration">
+                <?php if ($mobile_img) : ?>
+                    <source srcset="<?php echo esc_url($mobile_img['url']); ?>"
+                            media="(max-width: 428px)">
+                <?php endif; ?>
+
+                <img src="<?php echo esc_url($desktop_img['url']); ?>"
+                     alt="<?php echo esc_attr($desktop_img['alt']); ?>"
+                     class="programs-goals__decoration-img">
+            </picture>
+        <?php endif;
+    endif; ?>
+
+
     <div class="container">
         <div class="programs-goals__cards">
             <?php if (have_rows('program_cards')) : ?>
