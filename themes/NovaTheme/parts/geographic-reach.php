d12a34149403ef41ac8877482bcf7a3333d2604f
Alex <connectionwithme1@gmail.com>
2026-05-25 07:03:59 +0300

    wip section geographic reach

--- themes/NovaTheme/parts/geographic-reach.php
+++ themes/NovaTheme/parts/geographic-reach.php
@@ -8,4 +8,40 @@
         ) ?>
     <?php endif; ?>
 
+
+    <?php if (have_rows('map_markers')): ?>
+        <?php while (have_rows('map_markers')): the_row();
+            $country = get_sub_field('country');
+            $tooltip = get_sub_field('tooltip');
+            $x = get_sub_field('x_coordinate');
+            $y = get_sub_field('y_coordinate');
+            $marker = get_sub_field('marker'); ?>
+
+            <div class="geographic-reach__item"
+                 style="position: absolute;
+                         top: <?php echo $y; ?>%;
+                         left: <?php echo $x; ?>%;
+                         transform: translate(-50%, -50%);"
+            >
+                <?php if ($country): ?>
+                    <p><?php echo $country; ?></p>
+                <?php endif; ?>
+                <?php echo wp_get_attachment_image(
+                        $marker['ID'],
+                        'thumbnail',
+                        false,
+                        ['class' => 'geographic-reach__marker']
+                ) ?>
+                <?php if ($tooltip): ?>
+                    <div class="geographic-reach__tooltip">
+                        <?php echo $tooltip; ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+
+
+        <?php endwhile; ?>
+    <?php endif; ?>
+
+
 </section>
