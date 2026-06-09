8c51ed728d4fbc1fd742bccfe179469a2aa0b1d8
alex <connectionwithme1@gmail.com>
2026-06-09 12:39:14 +0000

    Getting Started About Us

--- themes/NovaTheme/src/js/main.jsx
+++ themes/NovaTheme/src/js/main.jsx
@@ -1,4 +1,5 @@
 import '../scss/main.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import './global.js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
---
+++ themes/NovaTheme/templates/templates-about-us.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: About Us
+ */
+
+get_header(); ?>
+
+<main>
+    <section class="about-hero">
+        <div class="full">
+            <div class="container">hello</div>
+        </div>
+    </section>
+</main>
+
+
+<?php get_footer(); ?>
+
