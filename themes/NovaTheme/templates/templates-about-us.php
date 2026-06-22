a1560e147972c6205391d2678fb2635c4a4d594d
alex <connectionwithme1@gmail.com>
2026-06-22 15:47:14 +0000

    necessary to work - 'How it works'

--- themes/NovaTheme/templates/templates-about-us.php
+++ themes/NovaTheme/templates/templates-about-us.php
@@ -5,10 +5,12 @@
 
 get_header(); ?>
 
-    <main>
-        <?php get_template_part('parts/about', 'hero' ); ?>
-        <?php get_template_part('parts/about', 'organization' ); ?>
-        <?php get_template_part('parts/about-colors', 'journey' ); ?>
-    </main>
+<main>
+	<?php get_template_part( 'parts/about', 'hero' ); ?>
+
+		<?php get_template_part( 'parts/about', 'organization' ); ?>
+	<?php get_template_part( 'parts/about-colors', 'journey' ); ?>
+	<?php get_template_part( 'parts/about-colors', 'reach' ); ?>
+</main>
 
 <?php get_footer(); ?>
